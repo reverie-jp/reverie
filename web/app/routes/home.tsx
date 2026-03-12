4912250728b97c7fc63be3b21be0286e20ee8bd7
ekkx <77382767+ekkx@users.noreply.github.com>
2026-03-12 09:20:51 +0000

    chore: setup web frontend

---
+++ web/app/routes/home.tsx
@@ -0,0 +1,13 @@
+import { Button } from "~/components/ui/button";
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <Button size="lg">Press me!</Button>;
+}
